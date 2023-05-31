eea226b364c67e1d2bc51cac57b9d5600d440a24
veronikaiva <135086752+veronikaiva@users.noreply.github.com>
2023-05-30 23:21:53 -0400

    ymnoshenie

--- Nika.c
+++ Nika.c
@@ -5,7 +5,9 @@ int main() {
 	int b = 3;
 	int sum = a + b;
 	int difference = a - b;
+	int ymnoshenie = a * b;
 	printf("summa: %d\n", sum);
 	printf("raznoct: %d\n", difference);
+	printf("ymnoshenie: %d\n", ymnoshenie);
 	return 0;
 }
